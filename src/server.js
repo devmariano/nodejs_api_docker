1859096529a7452e6ec7a8305c8a4dcdd6382969
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-10-02 07:30:15 -0300

    limpeza para testes jest

--- src/server.js
+++ src/server.js
@@ -1,27 +1,9 @@
 //forma correta de importar o express quando se trata de api
 const express = require('express');
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('api_docker', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3388,
-});
-
-const connect = async() => {
-    try {
-        await sequelize.authenticate();
-        console.log('Conexão estabelecida com sucesso');
-    }catch(error) {
-        console.error('Não foi possivel conectar ao DB', error)
-    }
-}
-connect();
-
-
 const server = express();
 
 server.use(express.json()); // nosso servidor vai expor os dados em formato json
+
 server.get('/', (req, res) => {
     res.json({ welcome: "Hello World, Alex" })
 })
